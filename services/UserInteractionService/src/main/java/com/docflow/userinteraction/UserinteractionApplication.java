53a9c53227327273df3ff32ca89ace99c229e740
m8-XD <m8-XD@proton.me>
2024-03-25 17:33:51 +0300

    feat: add docker compose script and add postgres init.sql file

---
+++ services/UserInteractionService/src/main/java/com/docflow/userinteraction/UserinteractionApplication.java
@@ -0,0 +1,12 @@
+package com.docflow.userinteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserinteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserinteractionApplication.class, args);
+	}
+}
---
+++ services/UserInteractionService/src/test/java/com/docflow/userinteraction/UserinteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docflow.userinteraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserinteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
