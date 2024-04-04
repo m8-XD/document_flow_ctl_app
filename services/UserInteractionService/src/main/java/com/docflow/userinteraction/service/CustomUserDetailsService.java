ecb6161488ce1e41f53988f9ee6278131cf9ed4d
m8-XD <m8-XD@proton.me>
2024-04-04 18:06:37 +0300

    fix: fix previous add admin page for managing users

--- services/UserInteractionService/src/main/java/com/docflow/userinteraction/service/CustomUserDetailsService.java
+++ services/UserInteractionService/src/main/java/com/docflow/userinteraction/service/CustomUserDetailsService.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
